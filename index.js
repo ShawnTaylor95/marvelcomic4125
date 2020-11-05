50d296e6fb924a6718e9e97677388d1ef29a032f
Ashlyn708 <averett_ashlyn@columbusstate.edu>
2020-11-05 14:37:47 +0000

    basic welcome and contact

--- index.js
+++ index.js
@@ -20,24 +20,37 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 //get home page /
 app.get('/', function(req, res){
-    let comicData;
-    fetch('http://xkcd.com/info.0.json')
-    .then(res => res.json())
-    .then(data => {
-        res.render('index',{data:data})
+    res.render('index')
     console.log("got it");
-    })
+    
+});
+
+app.get('/character', function(req, res){
+    res.render('character')
+    console.log("got it");
+    
+});
+
+app.get('/date', function(req, res){
+    res.render('date')
+    console.log("got it");
     
 });
 
 app.get('/random', function(req, res){
-    let randNum=Math.floor((Math.random() * 2373) + 1);
-    fetch('https://xkcd.com/'+randNum+'/info.0.json')
-    .then(res => res.json())
-    .then(data => {
+   // let randNum=Math.floor((Math.random() * 2373) + 1);
+    //fetch('https://xkcd.com/'+randNum+'/info.0.json')
+    //.then(res => res.json())
+    //.then(data => {
         res.render('random',{data:data})
-    });
+    //});
 })
+
+app.get('/contact', function(req, res){
+    res.render('contact')
+    console.log("got it");
+    
+});
 
 //app.post('/newRand', function(req, res){
   //  let randNum=Math.floor((Math.random() * 2373) + 1);
