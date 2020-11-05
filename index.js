675e68e4635ec61d59e3167ec1faf4f5d0d30994
ShawnTaylor95 <Taylor_Shawn2@Columbusstate.edu>
2020-11-05 15:51:43 +0000

    added apikey, jumbotron, navbar

--- index.js
+++ index.js
@@ -1,54 +1,59 @@
-//require express
 var express = require('express');
-//require body-parser
-var bodyParser = require("body-parser");
-//require node-fetch
 var fetch = require('node-fetch');
-//create express object, call express
 var app = express();
-//get port information
+var md5   = require("blueimp-md5");
+
+var pubkey = '94527016ce8d780d5741835efed6c566';
+var pvtkey = 'b0d94bb240304ac1daea677d8eefc358a55cb82f';
+var ts = new Date().getTime();
+var stringToHash  = ts+pvtkey+pubkey;
+var hash = md5(stringToHash);
+
+// url for characters
+var baseUrl = 'https://gateway.marvel.com:443/v1/public/characters';
+var limit = 20;
+var url = baseUrl + '?limit=' + limit + '&ts=' + ts + '&apikey=' + pubkey + '&hash=' + hash;
+
+//Port information
 const port = process.env.PORT || 3000;
 
-//tell application to use EJS for templates
+
+//tell application to use ejs for templates
 app.set('view engine', 'ejs');
 //make styles public
 app.use(express.static("public"));
-//tell app to use Body parser
-app.use(bodyParser.urlencoded({extended: true}));
 
+app.get('/', function(req,res){
+//return something to homepage
+    //res.render('index');
+    return res.redirect('/comic');
 
-
-//get home page /
-app.get('/', function(req, res){
-    let comicData;
-    fetch('http://xkcd.com/info.0.json')
-    .then(res => res.json())
-    .then(data => {
-        res.render('index',{data:data})
-    console.log("got it");
-    })
-    
 });
 
-app.get('/random', function(req, res){
-    let randNum=Math.floor((Math.random() * 2373) + 1);
-    fetch('https://xkcd.com/'+randNum+'/info.0.json')
+
+//fetches comic api data and send it to frontend of /comic
+app.get('/comic', function(req,res){
+    fetch(url)
     .then(res => res.json())
     .then(data => {
-        res.render('random',{data:data})
+        res.render('index', {data: data});
+        console.log(data);
     });
-})
+});
 
-//app.post('/newRand', function(req, res){
-  //  let randNum=Math.floor((Math.random() * 2373) + 1);
-    //fetch('https://xkcd.com/'+randNum+'/info.0.json')
-    //.then(res => res.json())
-    //.then(data => {
-      //  res.render('random',{data:data})
-    //});
-//})
+/*fetches random comic api data and send it to frontend of /rancComic
+app.get('/ranComic', function(req,res){
+    var ranNumber = Math.floor(Math.random() * 2373) + 1;
+    fetch('http://xkcd.com/'+ranNumber+'/info.0.json')
+    .then(res => res.json())
+    .then(randomData => {
+        res.render('ranComic', {randomData: randomData});
+    });
+});
+*/
 
-//server setup
-app.listen(port, function(){
+
+//Server setup
+app.listen(port,function(){
     console.log('Listening on ' + port)
 });
