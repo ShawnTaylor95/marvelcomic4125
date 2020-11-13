2942f46f1fdea509a066c256eaca80bddc6ff7a1
Alyssia21 <mahone_alyssia@columbusstate.edu>
2020-11-13 23:57:29 +0000

    heroku link

--- index.js
+++ index.js
@@ -18,35 +18,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 
 
-//get home page /
-app.get('/', function(req, res){
-    let comicData;
-    fetch('http://xkcd.com/info.0.json')
-    .then(res => res.json())
-    .then(data => {
-        res.render('index',{data:data})
-    console.log("got it");
-    })
-    
-});
 
-app.get('/random', function(req, res){
-    let randNum=Math.floor((Math.random() * 2373) + 1);
-    fetch('https://xkcd.com/'+randNum+'/info.0.json')
-    .then(res => res.json())
-    .then(data => {
-        res.render('random',{data:data})
-    });
-})
-
-//app.post('/newRand', function(req, res){
-  //  let randNum=Math.floor((Math.random() * 2373) + 1);
-    //fetch('https://xkcd.com/'+randNum+'/info.0.json')
-    //.then(res => res.json())
-    //.then(data => {
-      //  res.render('random',{data:data})
-    //});
-//})
 
 //server setup
 app.listen(port, function(){
