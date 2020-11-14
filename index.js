d560087b5d1b8ddc1cd3c272b4f8930eafa49165
Alyssia21 <mahone_alyssia@columbusstate.edu>
2020-11-14 03:12:21 +0000

    pages

--- index.js
+++ index.js
@@ -16,11 +16,52 @@ app.use(express.static("public"));
 //tell app to use Body parser
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', function(req,res){
+app.get('/', function(req, res){
     res.render('index')
-    console.log("got it")
+    console.log("got it");
+    
 });
 
+app.get('/character', function(req, res){ 
+    res.render('character')
+    console.log("got it");
+});
+
+app.post('/getCharacter',function(req,res){
+let data = marvel.characters
+    .name("Hulk")
+    .get(function(err, resp) {
+        if (err) { console.log("Error: ", err) }
+        else { console.log(resp)}
+    });
+    res.render('character',{data:data});
+
+});
+    
+
+
+app.get('/date', function(req, res){
+    res.render('date')
+    console.log("got it");
+    
+});
+
+app.get('/random', function(req, res){
+   // let randNum=Math.floor((Math.random() * 2373) + 1);
+    //fetch('https://xkcd.com/'+randNum+'/info.0.json')
+    //.then(res => res.json())
+    //.then(data => {
+        res.render('random')
+        console.log("got it");
+
+    //});
+})
+
+app.get('/contact', function(req, res){
+    res.render('contact')
+    console.log("got it");
+    
+});
 
 
 //server setup
