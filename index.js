ef6ea72dc8e291d4a58aa8b4435f957b2d6c80fa
Ashlyn708 <averett_ashlyn@columbusstate.edu>
2020-11-14 02:48:59 +0000

    working on marvel

--- index.js
+++ index.js
@@ -8,10 +8,8 @@ var fetch = require('node-fetch');
 var app = express();
 //creat marvel object,
 var api = require('marvel')
-
-
-
-
+//create
+var md5 = require("blueimp-md5")
 //get port information
 const port = process.env.PORT || 3000;
 
@@ -22,8 +20,14 @@ app.use(express.static("public"));
 //tell app to use Body parser
 app.use(bodyParser.urlencoded({extended: true}));
 // use marvel public and private key
-var marvel = new api({ publicKey: "e1e8485e01f4cb3811178c89ef33f8c6", 
-privateKey: "110a77fc5084cb96dc850208d085e60b7f3a4740"})
+//var marvel = new api({
+var  publicKey= "e1e8485e01f4cb3811178c89ef33f8c6"; 
+var privateKey= "110a77fc5084cb96dc850208d085e60b7f3a4740";
+var ts = new Date().getTime();
+var charLimit=3;
+var stringToHash = ts+privateKey+publicKey;
+var hash = md5(stringToHash);
+var data;
 
 //get home page /
 app.get('/', function(req, res){
@@ -32,18 +36,25 @@ app.get('/', function(req, res){
     
 });
 
-app.get('/character', function(req, res){
+app.get('/character', function(req, res){ 
     res.render('character')
-    marvel.characters
-    .name("Hulk")
-    .get(function(err, resp) {
-        if (err) { console.log("Error: ", err) }
-        else { console.log(resp)
-        data =>{resp} }
-    })
     console.log("got it");
-    
 });
+
+
+app.post('/getCharacter',function(req,res){
+    var name;
+ name=req.body.heroName;
+fetch('https://gateway.marvel.com:443/v1/public/characters?name='+ name+ '&limit='
+      + charLimit + '&ts=' + ts + '&apikey=' + publicKey + '&hash=' + hash)
+    .then(res => res.json())
+    .then(data => {	
+        res.results
+       res.render('character', {data: data});	
+        console.log(data);}
+)});
+    
+
 
 app.get('/date', function(req, res){
     res.render('date')
@@ -65,15 +76,6 @@ app.get('/contact', function(req, res){
     console.log("got it");
     
 });
-
-//app.post('/newRand', function(req, res){
-  //  let randNum=Math.floor((Math.random() * 2373) + 1);
-    //fetch('https://xkcd.com/'+randNum+'/info.0.json')
-    //.then(res => res.json())
-    //.then(data => {
-      //  res.render('random',{data:data})
-    //});
-//})
 
 //server setup
 app.listen(port, function(){
